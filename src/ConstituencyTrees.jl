95dd7b044e942ebb4634e03107a025e88b6203cf
David Ellison <davidehellison@gmail.com>
2019-03-29 20:52:19 -0700

    init

---
+++ src/ConstituencyTrees.jl
@@ -0,0 +1,7 @@
+module ConstituencyTrees
+
+using Reexport
+@reexport using AbstractTrees
+
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using ConstituencyTrees, Test
+
+@testset "ConstituencyTrees.jl" begin
+    @test 1 == 1
+end
